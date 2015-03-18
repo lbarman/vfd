6ee29caea227593cc736fcc0ac82aa3de2fa6609
Ludovic Barman <ludovic.barman@gmail.com>
2015-03-18 23:49:34 +0100

    Adds GALAXY color scheme and background, well with highlight

--- vfd-dashboard/src/main/scala/vfd/dashboard/ui/Layout.scala
+++ vfd-dashboard/src/main/scala/vfd/dashboard/ui/Layout.scala
@@ -1,44 +1,36 @@
 package vfd.dashboard.ui
 
-import rx.Obs
-import scalatags.JsDom.all._
+import org.scalajs.dom.HTMLElement
+
+import scalatags.JsDom.all.ExtendedString
+import scalatags.JsDom.all.bindNode
+import scalatags.JsDom.all.`class`
+import scalatags.JsDom.all.div
+import scalatags.JsDom.all.height
+import scalatags.JsDom.all.iframe
+import scalatags.JsDom.all.p
+import scalatags.JsDom.all.src
+import scalatags.JsDom.all.stringAttr
+import scalatags.JsDom.all.stringFrag
+import scalatags.JsDom.all.stringStyle
+import scalatags.JsDom.all.style
+import scalatags.JsDom.all.width
 import vfd.dashboard.Environment
 import vfd.dashboard.MavlinkSocket
-import vfd.dashboard.ui.instruments.Altimeter
-import vfd.dashboard.ui.instruments.Balance
-import vfd.dashboard.ui.instruments.Bar
-import vfd.dashboard.ui.instruments.Compass
-import vfd.dashboard.ui.instruments.Generic
-import vfd.dashboard.ui.instruments.Horizon
-import org.mavlink.messages.Heartbeat
-import org.mavlink.messages.Attitude
-import vfd.dashboard.ui.instruments.Clock
+import vfd.dashboard.ui.panels.Communication
+import vfd.dashboard.ui.panels.Primary
 
-class Layout(socket: MavlinkSocket)(implicit env: Environment) {
+class Layout(socket: MavlinkSocket) {
 
-  private def panel(contents: Frag*) = div(`class` := "d-panel")(contents: _*)
-
-  private def mode(name: String, kind: String, on: Boolean = false) = div(`class` := s"mode $kind ${if (!on) "off"}")(name)
-
-  val modes = div(
-    mode("MANUAL", "warning", true),
-    mode("STABILIZED", "info", true),
-    mode("GUIDED", "success", true),
-    mode("AUTO", "success", true))
-
-  val infos = div(
-    mode("BAY", "info"),
-    mode("RECOVERY", "danger"),
-    mode("NOGPS", "warning", true),
-    mode("OVERLOAD", "danger", true),
-    mode("BATTERY", "danger", false),
-    mode("LINK", "danger", true),
-    mode("SOCKET", "danger", true),
-    div(style := "float: right")(mode("CRITICAL", "danger", true)))
+  def well(content: String) = 
+        div(`class` := "panel panel-default well")(
+          div(`class` := "well-header")(div(`class` := "well-title")("Title")),
+          div(`class` := "panel-body well-inner")(
+            content))
 
   val map = iframe(
-    width := 100.pct,
-    height := 350.px,
+    width := "100%",
+    height := "350px",
     "frameborder".attr := "0",
     "scrolling".attr := "no",
     "marginheight".attr := "0",
@@ -48,100 +40,18 @@ class Layout(socket: MavlinkSocket)(implicit env: Environment) {
   val feed = div(style := "width: 100%; height: 460px; color: #ffffff; background-color: #c2c2c2; text-align: center;")(
     p(style := "padding-top: 220px")("video feed"))
 
-  val altimeter = new Altimeter
-  val horizon = new Horizon
-  val compass = new Compass
-  val motor0 = new Generic(0, 50, 100, "%")
-  val motor1 = new Generic(0, 50, 100, "%")
-  val motor2 = new Generic(0, 50, 100, "%")
-  val motor3 = new Generic(0, 50, 100, "%")
-  val powerDistribution = new Balance()
-  val batteryLevel = new Bar()
-
-  val top = header(
-    div("Flight Control Panel"),
-    div((new Clock).element),
-    div("System #"))
-
-  val left = panel(
-    map,
-    table(`class` := "table-instrument")(
-      thead("Motors"),
-      tbody(
-        tr(
-          td(motor0.element),
-          td(),
-          td(motor1.element),
-          td()
-        ),
-        tr(
-          td(),
-          td(powerDistribution.element),
-          td(),
-          td()
-        ),
-        tr(
-          td(motor2.element),
-          td(),
-          td(motor3.element),
-          td()
-        )
-      )
-    )
-  )
-
-  val center = panel(feed)
-
-  val below = panel(
-    table(`class` := "table-instrument")(
-      tbody(
-        tr(
-          td(compass.element),
-          td(horizon.element),
-          td(altimeter.element)
-        )
-      )
-    )
-  )
-
-  val right = panel()
-
-  val element = div(`class` := "d-container d-column")(
-    div(`class` := "d-above")(
-      top),
-    div(`class` := "d-above d-container d-row")(
-      panel(modes),
-      panel(infos)),
-    div(`class` := "d-container d-row")(
-      div(`class` := "d-container d-details")(
-        panel("foo")),
-      div(`class` := "d-container d-left")(
-        left),
-      div(`class` := "d-container d-column d-middle")(
-        div(`class` := "d-container d-center")(
-          center),
-        div(`class` := "d-container d-below")(
-          below)
-      ),
-      div(`class` := "d-container d-right")(
-        right
-      )
-    )
-  ).render
-
-  //message router
-  Obs(socket.message, skipInitial = true) {
-    socket.message() match {
-      
-      case att: Attitude =>
-        horizon.value() = (att.pitch, att.roll)
-        compass.value() = att.yaw
-
-      //TODO route other messages
-
-      case _ => ()
-      
-    }
-  }
+  def element(implicit env: Environment): HTMLElement = div(`class` := "container-fluid")(
+    div(`class` := "row")(
+      div(`class` := "col-xs-4")(
+        well("test")),
+      div(`class` := "col-xs-5")(
+        div(`class` := "panel panel-default")(
+          div(`class` := "panel-body")(
+            feed)),
+        div(`class` := "panel panel-default")(
+          div(`class` := "panel-body")(Primary(socket)))),
+      div(`class` := "col-xs-3")(
+        div(`class` := "panel panel-default")(
+          div(`class` := "panel-body")(Communication(socket)))))).render
 
 }
